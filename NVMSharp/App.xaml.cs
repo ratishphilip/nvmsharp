68aee565c0517b2a86bc781db6eef3ca8eaa1568
AlfredBr <alfredbr@outlook.com>
2015-11-18 18:53:58 -0500

    Added SingleInstance handler to limit the application to a single running instance

--- NVMSharp/App.xaml.cs
+++ NVMSharp/App.xaml.cs
@@ -1,17 +1,52 @@
-﻿using System.Windows;
-using NVMSharp.Services;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Microsoft.Shell;
 
 namespace NVMSharp
 {
     /// <summary>
     /// Interaction logic for App.xaml
+    /// 
+    /// See: http://blogs.microsoft.co.il/arik/2010/05/28/wpf-single-instance-application/
+    /// 
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
-        public App()
+        private const string UniqueName = @"{C4F588CD-D652-4329-8FE3-8F73CA2B7E04}";
+        private static App _application;
+
+        [STAThread]
+        public static void Main()
         {
-            // Inject the required services into the ServiceManager
-            ServiceInjector.InjectServices();
+            if (SingleInstance<App>.InitializeAsFirstInstance(UniqueName))
+            {
+                _application = new App();
+
+                _application.InitializeComponent();
+                _application.Run();
+
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
         }
+
+        #region ISingleInstanceApp Members
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // this is executed in the original instance
+
+            // we get the arguments to the second instance and can send them to the existing instance if desired
+
+            // here we bring the existing instance to the front
+            _application.MainWindow.BringToFront();
+
+            // handle command line arguments of second instance
+            
+            return true;
+        }
+
+        #endregion
     }
 }
